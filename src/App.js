b9cef95db420a26e79ecfaf9c3b0756559da7321
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-27 11:58:26 +0200

    430. Styled Components - Basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import Landing from './pages/Landing'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 2rem;
+`
+const SecondButton = styled.button`
+  background: blue;
+  color: yellow;
+  font-size: 3rem;
+`
 
 const App = () => {
   return (
     <div>
+      <Button>click me</Button>
+      <SecondButton>click me</SecondButton>
       <Landing />
     </div>
   )
