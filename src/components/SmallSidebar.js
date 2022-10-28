3a3aaa64c76152bd9611f0e463383d4c4c1ef7e3
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-28 12:05:33 +0200

    468. Small Sidebar - Nav Links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -4,6 +4,7 @@ import { FaTimes } from 'react-icons/fa'
 import { Logo } from '.'
 import { toggleSidebar } from '../features/user/userSlice'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import links from '../utils/links'
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user)
@@ -23,7 +24,26 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>nav links</div>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { id, text, path, icon } = link
+
+              return (
+                <NavLink
+                  key={id}
+                  to={path}
+                  end
+                  className={({ isActive }) =>
+                    `nav-link ${isActive && 'active'}`
+                  }
+                  onClick={toggle}
+                >
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              )
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
