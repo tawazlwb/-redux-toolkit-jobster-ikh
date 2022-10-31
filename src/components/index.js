6ed8acbec5279cbb13e9e196a6d8a5057253228d
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-31 09:19:24 +0100

    490. AllJobs Page Structure

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,9 @@ import SmallSidebar from './SmallSidebar'
 import Navbar from './Navbar'
 import NavLinks from './NavLinks'
 import FormRowSelect from './FormRowSelect'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
+import Job from './Job'
 
 export {
   Logo,
@@ -14,4 +17,7 @@ export {
   Navbar,
   NavLinks,
   FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  Job,
 }
---
+++ src/features/allJobs/allJobsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+import customFetch from '../../utils/axios'
+
+const initialFiltersState = {
+  search: '',
+  searchStatus: 'all',
+  searchType: 'all',
+  sort: 'latest',
+  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+}
+
+const initialState = {
+  isLoading: false,
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
+  stats: {},
+  monthlyApplications: [],
+  ...initialFiltersState,
+}
+
+const allJobsSlice = createSlice({
+  name: 'allJobs',
+  initialState,
+  reducers: {},
+  extraReducers: {},
+})
+
+// export const {} = allJobsSlice.actions
+
+export default allJobsSlice.reducer
